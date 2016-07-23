e0b7fb3032fdf88df555cad7451b761719c2606e
Boris Nagaev <bnagaev@gmail.com>
2016-07-23 23:56:40 +0300

    fetch numbers by HTTP

--- main.go
+++ main.go
@@ -2,29 +2,45 @@ package main
 
 import(
     "fmt"
+    "net/http"
 
     "github.com/starius/httpheap/chanheap"
 )
 
+func ReadHttpInts(url string, channel chan<- int) {
+    defer close(channel)
+    res, err := http.Get(url)
+    if err != nil {
+        return
+    }
+    defer res.Body.Close()
+    for {
+        var value int
+        _, err := fmt.Fscanf(res.Body, "%d\n", &value)
+        if err != nil {
+            break
+        }
+        channel <- value
+    }
+}
+
 func main() {
-    c1 := make(chan int)
-    c2 := make(chan int)
-    go func() {
-        c1 <- 0
-        c1 <- 10
-        c1 <- 20
-        close(c1)
-    }()
-    go func() {
-        c2 <- 5
-        c2 <- 15
-        c2 <- 25
-        close(c2)
-    }()
+    const N = 10
+    chans := make([]chan int, 0)
+    for step := 1; step <= N; step++ {
+        url := fmt.Sprintf(
+            "http://127.0.0.1:25516/ints/?start=1&stop=100&step=%d",
+            step,
+        )
+        channel := make(chan int)
+        chans = append(chans, channel)
+        go ReadHttpInts(url, channel)
+    }
 
     chan_heap := new(chanheap.ChanHeap)
-    chan_heap.AddChan(c1)
-    chan_heap.AddChan(c2)
+    for _, channel := range chans {
+        chan_heap.AddChan(channel)
+    }
     for {
         value, has_value := chan_heap.PopValue()
         if !has_value {
